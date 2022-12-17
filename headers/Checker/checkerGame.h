7f59c2ceb60e78bd5fb7d9478200bd8556a91da6
Melissa Gries (CadenEras) <gries.melissa@outlook.fr>
2022-12-17 14:40:45 +0100

    Finished checkerGame.h

---
+++ headers/Checker/checkerGame.h
@@ -0,0 +1,27 @@
+//
+// Created by gries on 16/12/2022.
+//
+
+#ifndef C_PAS_COOL_GAMEPLATE_CHECKERGAME_H
+#define C_PAS_COOL_GAMEPLATE_CHECKERGAME_H
+
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "interface.h"
+#include "bases.h"
+
+void initGame(SDL_Surface *screen);
+Position getMousePosition(SDL_Surface *screen);
+int isPieceSelected(int turn, GridType grid[][8], Position mousePosition);
+int isDestinationValid(int turn, GridType grid[][8], Position from, Position dest);
+void updateGrid(int turn, GridType grid[][8], Position from, Position dest);
+int removePiece(int turn, GridType grid[][8], Position from, Position dest);
+int whoWin(GridType grid[][8]);
+
+#endif //C_PAS_COOL_GAMEPLATE_CHECKERGAME_H
