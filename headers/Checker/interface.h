2acda9b395ed07ba78208303da78f349e65539ea
Melissa Gries (CadenEras) <gries.melissa@outlook.fr>
2022-12-17 14:31:56 +0100

    Finished interface.h

---
+++ headers/Checker/interface.h
@@ -0,0 +1,29 @@
+//
+// Created by gries on 16/12/2022.
+//
+
+#ifndef C_PAS_COOL_GAMEPLATE_INTERFACE_H
+#define C_PAS_COOL_GAMEPLATE_INTERFACE_H
+
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "bases.h"
+
+enum HighlighterType {
+	HIGHLIGHTER_TYPE_NONE,
+	HIGHLIGHTER_TYPE_MOUSE_OVER,
+	HIGHLIGHTER_TYPE_MOVALBE,
+	HIGHLIGHTER_TYPE_SELECTED,
+	HIGHLIGHTER_TYPE_NEXT_POSSIBLE_POSITION,
+};
+
+void printBoard(SDL_Surface *screen, GridType grid[][8]);
+void initBoard(GridType grid[][8]);
+
+#endif //C_PAS_COOL_GAMEPLATE_INTERFACE_H
